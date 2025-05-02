34b30b1f2696f15b5e913a4a52287d8aabff0ac7
mooolalina <kataprocenko55@gmail.com>
2025-05-02 19:52:11 +0400

    расширение скрипта

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -1,19 +1,37 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; 
+
 [RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
 public class PlayerController : MonoBehaviour
 {
-[SerializeField] private Rigidbody _rigidbody;
-[SerializeField] private FixedJoystick _joystick;
-[SerializeField] private float _moveSpeed;
-private void FixedUpdate()
-{
-_rigidbody.linearVelocity = new Vector3(_joystick.Horizontal * _moveSpeed, _rigidbody.linearVelocity.y, _joystick.Vertical * _moveSpeed);
-if (_joystick.Horizontal != 0 || _joystick.Vertical != 0)
-{
-transform.rotation = Quaternion.LookRotation(_rigidbody.linearVelocity);
-}
-}
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private FixedJoystick _joystick;
+    [SerializeField] private float _moveSpeed;
+
+    [SerializeField] private TextMeshProUGUI forceText; 
+    public int health = 0; 
+
+    private void FixedUpdate()
+    {
+        _rigidbody.linearVelocity = new Vector3(_joystick.Horizontal * _moveSpeed, _rigidbody.linearVelocity.y, _joystick.Vertical * _moveSpeed);
+        
+        if (_joystick.Horizontal != 0 || _joystick.Vertical != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(_rigidbody.linearVelocity);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Friend"))
+        {
+            Friend friend = collision.gameObject.GetComponent<Friend>();
+            int healthFriend = friend._health; 
+            health += healthFriend;
+            forceText.text = health.ToString(); 
+            Destroy(collision.gameObject); 
+        }
+    }
 }
