43cbea287ad49410ee659459a7ac2ef5100bcc2d
mooolalina <kataprocenko55@gmail.com>
2025-05-04 16:36:43 +0400

    скрипт для друга

--- Scripts/Friend.cs
+++ Scripts/Friend.cs
@@ -1,6 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Friend : MonoBehaviour
 {
-    public int _health = 10; 
+    public int _health; 
+    [SerializeField] private TextMeshProUGUI healthText;
+
+
+    void Start()
+    {
+        _health = Random.Range(1, 11); 
+        UpdateHealthText();
+    }
+
+    private void UpdateHealthText()
+    {
+        if (healthText != null)
+        {
+            healthText.text = _health.ToString(); 
+        }
+        else
+        {
+            Debug.LogError("healthText не присоединен в инспекторе!");
+        }
+    }
+
+    void Update()
+    {
+    }
 }
