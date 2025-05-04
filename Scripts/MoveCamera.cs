4cd730ec55a3bc15c07ddb2ae9fe794cad82f0a3
mooolalina <kataprocenko55@gmail.com>
2025-05-04 14:26:18 +0400

    настройка камеры

--- Scripts/MoveCamera.cs
+++ Scripts/MoveCamera.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class MoveCamera : MonoBehaviour
 {
-private Transform player;
-public float camPositionSpeed = 5f;
-public Vector3 offset;
-void Start()
-{
-player = GameObject.FindGameObjectWithTag("Player").transform;
-}
-void FixedUpdate()
-{
-Vector3 newCamPosition = new Vector3(player.position.x+offset.x, offset.y, player.position.z + offset.z);
-transform.position = Vector3.Lerp(transform.position, newCamPosition, camPositionSpeed * Time.deltaTime);
-}
+    private Transform player;
+    public float camPositionSpeed = 5f; 
+    public Vector3 offset; 
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void LateUpdate() 
+    {
+        Vector3 newCamPosition = player.position + offset;
+        transform.position = Vector3.Lerp(transform.position, newCamPosition, camPositionSpeed * Time.deltaTime);
+    }
 }
