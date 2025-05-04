e23a0fffdbc0fac3a498a211912134dc36671d1a
mooolalina <kataprocenko55@gmail.com>
2025-05-04 18:14:35 +0400

    добавление сцены главного меню

---
+++ Scripts/LoadScenes.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenes : MonoBehaviour
+{
+    public void ChangeScenes(int _number)
+    {
+        SceneManager.LoadScene(_number);
+    }
+}
