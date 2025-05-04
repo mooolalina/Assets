769d90b734ab1ab0d4f30820660ae2cbd732ab3f
mooolalina <kataprocenko55@gmail.com>
2025-05-04 17:54:04 +0400

    доработка скрипта Enemy

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -3,83 +3,67 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class Enemy : MonoBehaviour
-{
-    public int _health_enemy; // Здоровье противника
-    [SerializeField] private TextMeshProUGUI enemyhealthText; // Текстовое поле для отображения здоровья
-    private GameObject[] friends; // Массив объектов типа Friend
-    private GameObject player; // Объект игрока
-    [SerializeField] private PlayerController playerController; // Ссылка на контроллер игрока
-    private GameObject closestFriend; // Ближайший объект Friend
-    public GameObject nearest; // Для удобства разработки
-    [SerializeField] private float _enemySpeed; // Скорость движения противника
+public class Enemy : MonoBehaviour {
+    public int _health_enemy; 
+    [SerializeField] private TextMeshProUGUI enemyhealthText;
+    private GameObject[] friends;
+    private GameObject player; 
+    [SerializeField] private PlayerController playerController; 
+    private GameObject closestFriend; 
+    public GameObject nearest; 
+    [SerializeField] private float _enemySpeed; 
 
-    private void Start()
-    {
-        // Установка стартового здоровья противника
-        _health_enemy = Random.Range(1, 51); // Здоровье от 1 до 50
-        enemyhealthText.text = _health_enemy.ToString(); // Отображение здоровья
-        friends = GameObject.FindGameObjectsWithTag("Friend"); // Получение всех объектов Friend
+    private void Start() {
+        _health_enemy = Random.Range(1, 51);
+        enemyhealthText.text = _health_enemy.ToString(); 
+        friends = GameObject.FindGameObjectsWithTag("Friend");
     }
 
-    void Update()
-    {
-        player = GameObject.FindGameObjectWithTag("Player"); // Получение объекта игрока
-        int playerHealth = playerController.health; // Получение здоровья игрока
+    void Update() {
+        player = GameObject.FindGameObjectWithTag("Player"); 
+        int playerHealth = playerController.health; 
 
-        // Движение к игроку, если здоровье противника больше здоровья игрока
-        if (_health_enemy > playerHealth)
-        {
+        if (_health_enemy > playerHealth) {
             transform.position = Vector3.MoveTowards(transform.position, player.transform.position, _enemySpeed * Time.deltaTime);
         }
-        else
-        {
-            // Обновление массива друзей
+        else {
             friends = GameObject.FindGameObjectsWithTag("Friend");
-            nearest = FindClosestFriend(); // Поиск ближайшего объекта Friend
-            if (nearest != null)
-            {
+            nearest = FindClosestFriend(); 
+            if (nearest != null) {
                 transform.position = Vector3.MoveTowards(transform.position, nearest.transform.position, _enemySpeed * Time.deltaTime);
             }
         }
     }
 
-    GameObject FindClosestFriend()
-    {
-        float distance = Mathf.Infinity; // Начальное значение для расстояния
-        Vector3 position = transform.position; // Позиция противника
-        foreach (GameObject go in friends)
-        {
-            Vector3 diff = go.transform.position - position; // Разница между позициями
-            float curDistance = diff.sqrMagnitude; // Квадрат расстояния
-            if (curDistance < distance)
-            {
-                closestFriend = go; // Запоминаем ближайшего друга
-                distance = curDistance; // Обновляем минимальное расстояние
+    GameObject FindClosestFriend() {
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position; 
+        foreach (GameObject go in friends) {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude; 
+            if (curDistance < distance) {
+                closestFriend = go; 
+                distance = curDistance; 
             }
         }
-        return closestFriend; // Возвращаем ближайшего друга
+        return closestFriend; 
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Friend")) // Проверка на столкновение с объектом Friend
-        {
-            Friend friend = collision.gameObject.GetComponent<Friend>(); // Получаем компонент Friend
-            int healthFriend = friend._health; // Получаем здоровье друга
-            _health_enemy += healthFriend; // Увеличиваем здоровье противника
-            enemyhealthText.text = _health_enemy.ToString(); // Обновляем текст здоровья
-            Destroy(collision.gameObject); // Уничтожаем объект Friend
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Friend")) {
+            Friend friend = collision.gameObject.GetComponent<Friend>(); 
+            int healthFriend = friend._health; 
+            _health_enemy += healthFriend; 
+            enemyhealthText.text = _health_enemy.ToString(); 
+            Destroy(collision.gameObject); 
         }
     }
 
-    public bool IsAlive()
-    {
-        return _health_enemy > 0; // Проверка, жив ли противник
+    public bool IsAlive() {
+        return _health_enemy > 0; 
     }
 
-    public void Hit(int damage)
-    {
-        _health_enemy -= damage; // Уменьшение здоровья противника
+    public void Hit(int damage) {
+        _health_enemy -= damage; 
     }
 }
